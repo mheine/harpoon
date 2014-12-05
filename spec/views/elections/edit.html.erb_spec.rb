ccd2fcb925c2a760ccf4736e4107cc7d402aa5f4
Martin Frost <frost@ceri.se>
2014-12-05 11:06:53 +0100

    added the missing rspec files for election scaffold

---
+++ spec/views/elections/edit.html.erb_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe "elections/edit", :type => :view do
+  before(:each) do
+    @election = assign(:election, Election.create!(
+      :name => "MyString",
+      :description => "MyText"
+    ))
+  end
+
+  it "renders the edit election form" do
+    render
+
+    assert_select "form[action=?][method=?]", election_path(@election), "post" do
+
+      assert_select "input#election_name[name=?]", "election[name]"
+
+      assert_select "textarea#election_description[name=?]", "election[description]"
+    end
+  end
+end
