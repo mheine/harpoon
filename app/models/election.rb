e0e484ab389cee4f026b6f6b30df4e9bfbae9fd2
Martin Frost <frost@ceri.se>
2014-12-11 19:30:55 +0100

    Write model specs for Election validations

--- app/models/election.rb
+++ app/models/election.rb
@@ -1,2 +1,3 @@
 class Election < ActiveRecord::Base
+  validates :name, :occurs_on, :published_on, :description, presence: true
 end
--- spec/models/election_spec.rb
+++ spec/models/election_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Election, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "validations" do
+    subject { Election.new }
+
+    required_attributes = %w[name description occurs_on published_on]
+    required_attributes.each do |attr|
+      it "requires a value for #{attr}" do
+        expect(subject).to be_invalid
+        expect(subject.errors).not_to be_empty
+        expect(subject.errors.keys).to include(attr.to_sym)
+      end
+    end
+  end
 end
